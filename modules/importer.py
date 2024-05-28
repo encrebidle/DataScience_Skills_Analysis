c4db9434695e9a148646eb4f3ff209f80de0d1d0
Atulya <53713477+encrebidle@users.noreply.github.com>
2024-05-29 05:19:05 +0530

    Update importer.py

--- modules/importer.py
+++ modules/importer.py
@@ -11,7 +11,7 @@ class DataImport:
     @st.cache_data(ttl=60*60) # ttl of one hour to keep memory in cache
     def fetch_and_clean_data():
         #data_url = 'https://storage.googleapis.com/gsearch_share/gsearch_jobs.csv'
-        data_url = 'data_source/gsearch_jobs.csv'
+        data_url = 'https://drive.google.com/file/d/1oirvdakkwsLmuIIdnMA14b654Vn-uxbr/view?usp=sharing'
         jobs_data = pd.read_csv(data_url).replace("'","", regex=True)
         jobs_data.date_time = pd.to_datetime(jobs_data.date_time)
         jobs_data = jobs_data.drop(labels=['Unnamed: 0', 'index'], axis=1, errors='ignore')
